5af7dea325c6bd5101f3453900b679a11b6e78b4
Iraponan Marinho <iraponan.aires@gmail.com>
2024-02-03 14:43:03 -0300

    Iniciando o firebase ao projeto.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseFirestore.instance.collection('col').doc('doc').set({'texto' : 'iraponan'});
   runApp(const MyApp());
 }
 
